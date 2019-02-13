72ccbb6787e64a9280cf13befc946da8c35d6e79
Laura Royle <lauraamie.royle@gmail.com>
2019-02-13 14:19:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -91,13 +91,13 @@ end
 
 def big_shoe_rebounds
     biggest_shoe_player = nil
-  
+
     game_hash.each do |location, team_data|
       team_data[:players].each do |stats|
-        if biggest_shoe_player == nil 
+        if biggest_shoe_player == nil
           biggest_shoe_player = stats
         end
-        
+
       end
     end
   end
